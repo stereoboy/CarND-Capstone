0a1d0319d8221455b110da9319f950b0a639c416
Jaeyoung Lee <jylee0121@gmail.com>
2018-01-25 20:29:37 +0900

    implement pid controller for steering
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,6 +10,9 @@ K_P = 0.3
 K_I = 0.005 # K_I is important because this throttle control is biased!
 K_D = 0.1
 
+YAW_K_P = 0.7
+YAW_K_I = 0.000
+YAW_K_D = 0.1
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit,
                     wheel_radius, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
@@ -24,12 +27,13 @@ class Controller(object):
         self.lowpass_filter = LowPassFilter(1.0, 3.0)
         self.reset_throttle_ctrl()
         self.yaw_ctrl = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
+        self.yaw_adjust_ctrl = PID(kp=YAW_K_P, ki=YAW_K_I, kd=YAW_K_D, mn=-0.3, mx=0.3)
         self.last_stamp = None
 
     def reset_throttle_ctrl(self):
         self.throttle_ctrl = PID(kp=K_P, ki=K_I, kd=K_D, mn=self.decel_limit, mx=self.accel_limit)
 
-    def control(self, linear_velocity, angular_velocity, current_velocity, current_stamp, dbw_status):
+    def control(self, linear_velocity, angular_velocity, current_linear_velocity, current_angular_velocity, current_stamp, dbw_status):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         if self.last_stamp is None:
@@ -37,10 +41,10 @@ class Controller(object):
         else:
             sample_time = (current_stamp - self.last_stamp)
         self.last_stamp = current_stamp
-        rospy.loginfo("error: {} - {} = {}".format(linear_velocity, current_velocity, linear_velocity - current_velocity))
+        rospy.loginfo("error: {} - {} = {}".format(linear_velocity, current_linear_velocity, linear_velocity - current_linear_velocity))
         rospy.loginfo("sample_time: {}".format(sample_time))
 
-        error = linear_velocity - current_velocity
+        error = linear_velocity - current_linear_velocity
         accel = self.throttle_ctrl.step(error, sample_time)
         rospy.loginfo("before lowpassfilter: {}".format(accel))
         accel = self.lowpass_filter.filt(accel)
@@ -64,5 +68,9 @@ class Controller(object):
             self.reset_throttle_ctrl()
 
         rospy.loginfo("\t\t\t\t\t\t\t\t\t(throttle, brake): {}\t\t\t{}".format(throttle, brake))
-        steer = self.yaw_ctrl.get_steering(linear_velocity, angular_velocity, current_velocity)
+        steer = self.yaw_ctrl.get_steering(linear_velocity, angular_velocity, current_linear_velocity)
+        print("############################################")
+        angular_error = current_angular_velocity = angular_velocity
+        steer = steer + self.yaw_adjust_ctrl.step(angular_error, sample_time)
+        print("############################################")
         return throttle, brake, steer
